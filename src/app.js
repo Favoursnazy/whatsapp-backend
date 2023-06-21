6875ef77160ffcbcb17a81a130e4b460f5c9a467
Favoursnazy4u <favoursnazy4u@gmail.com>
2023-06-21 20:50:07 +0100

    setup coss-env, adding middwares

--- src/app.js
+++ src/app.js
@@ -1,7 +1,49 @@
 import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import fileUpload from "express-fileupload";
+import cors from "cors";
+
+//dotenv config
+dotenv.config();
 
 //create express app
 const app = express();
+
+//morgan
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
+}
+
+//helmet
+app.use(helmet());
+
+//parse json request body and url
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//snaitize request data
+app.use(mongoSanitize());
+
+//enable cookie parser
+app.use(cookieParser());
+
+//gzip compression
+app.use(compression());
+
+//file upload
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
+
+//cors
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("hello from server");
